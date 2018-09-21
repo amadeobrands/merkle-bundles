a371d62d7adb7dc11a83ec59045a8638fb1fe2c9
liamzebedee <liamzebedee@yahoo.com.au>
2018-09-21 22:24:40 +0200

    client tests are working

--- client/test/index.ts
+++ client/test/index.ts
@@ -1,6 +1,8 @@
 import * as chai from 'chai';
-import chaiAsPromised from "chai-as-promised";
-chai.use(chaiAsPromised);
+
+import Dexie from 'dexie';
+
+chai.use(require("chai-as-promised"));
 const expect = chai.expect;
 const assert = chai.assert;
 
@@ -12,8 +14,6 @@ import {
 
 import { IDBBundleCacher } from '../../client/src/cache/indexeddb';
 
-import { cd, cp, rm, ls } from 'shelljs';
-
 
 describe('BundleLoader', function() {
     // const basedir = resolve(__dirname, "../resources/page1/dist");
@@ -23,15 +23,15 @@ describe('BundleLoader', function() {
     //     this.context.getTmpDir();
     // })
 
-    it('#new', async function() {
+    it('#new', function() {
         let loader = new BundleLoader("");
         expect(loader.cacher).to.not.be.null;
     });
 })
 
 describe('BundleCacher', function() {
-    it("#new", async function() {
+    it("#new", function() {
         let cacher = new IDBBundleCacher();
-        expect(cacher.db).to.not.be.null;
+        expect(cacher.db).to.be.an.instanceof(Dexie);
     })
 })
